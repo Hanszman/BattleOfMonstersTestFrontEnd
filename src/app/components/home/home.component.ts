6747b950877cbc0dfd95d6aaf87a26b3497b7a76
hanszman <victor.hanszman@hotmail.com>
2022-11-26 15:16:56 -0300

    criação de novas funções

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,32 +8,39 @@ import { ConfigClass } from '../../classes/config-class';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  listMonsters: any;
-  imgDir = '../../../assets/imgs/';
-  server: String = ConfigClass.getUrlApi().toString();
+    server: String = ConfigClass.getUrlApi().toString();
+    imgDir = '../../../assets/imgs/';
+    listMonsters: any;
+    monsterSelected: boolean = false;
 
-  constructor(private battleService: BattleService) { }
+    constructor(private battleService: BattleService) { }
 
-  ngOnInit(): void {
-    this.loadMonsters();
-  }
+    ngOnInit(): void {
+        this.loadMonsters();
+    }
 
-  loadMonsters() {
-    this.battleService.getTodos().subscribe(resp => {
-        if (resp && resp.body && resp.body.dados) {
-            this.listMonsters = resp.body.dados;
-            console.log(this.listMonsters);
-        } else {
-            console.log('No response from API.');
-        }        
-    });
-  }
-  
-  showMonsterData() {
+    loadMonsters() {
+        this.battleService.getTodos().subscribe(resp => {
+            if (resp && resp.body && resp.body.dados) {
+                this.listMonsters = resp.body.dados;
+                console.log(this.listMonsters);
+            } else {
+                console.log('No response from API.');
+            }        
+        });
+    }
 
-  }
+    selectPlayersMonster(index: any) {
+        console.log(index);
+        this.monsterSelected = true;
+        this.selectComputersMonster();
+    }
 
-  startBattle() {
-    console.log('startBattle');
-  }
+    selectComputersMonster() {
+
+    }
+
+    startBattle() {
+        console.log('startBattle');
+    }
 }
