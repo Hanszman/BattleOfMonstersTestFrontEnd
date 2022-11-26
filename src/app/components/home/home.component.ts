dee4da71ac8c02678b084133e7999905ac5279dc
hanszman <victor.hanszman@hotmail.com>
2022-11-26 11:28:02 -0300

    correção

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -5,7 +5,7 @@ import { ConfigClass } from '../../classes/config-class';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
   listMonsters: any;
